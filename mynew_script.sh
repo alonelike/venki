be7b31c6fe6927ae8e6a8f7015bff3aec9f25bcf
venki <you@example.com>
2021-11-29 00:54:23 -0800

    test update

--- mynew_script.sh
+++ mynew_script.sh
@@ -7,6 +7,13 @@ pipeline {
                 echo 'Hello World'
 		echo 'how are you'
             }
+    	stage('testing mode'){
+	    steps {
+	    	echo 'testing is completed'
+
         }
     }
 }
+}
+}
+

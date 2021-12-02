9ac0c689a47e9d0081b0d05c73529a92c00bd588
venki <you@example.com>
2021-12-02 08:08:22 -0800

    new 2changes

--- mynew_script.sh
+++ mynew_script.sh
@@ -1,20 +1,23 @@
 pipeline {
     agent any
 
-    stages {
-        stage('Hello') {
-            steps {
-                echo 'Hello World'
-		echo 'how are you'
-            }
-    	stage('testing mode') {
-		    steps {
-	    		echo 'testing is completed'
-	    	    }
-    	}
-}
-}
-}
+    		stages {
+       			 stage('Hello') {
+
+            			steps {
+                			echo 'Hello World'
+              				echo 'how are you'
+         		  	}
+      			 stage('testing mode') {
+
+                    		steps {
+                        		echo 'testing is completed'
+                   		 }
+			 }
+
+		   	}
+	}
+
     	
 
 
